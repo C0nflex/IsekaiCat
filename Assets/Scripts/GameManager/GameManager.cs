44a2fb8163f35b15a6f30160f85a31756dce2769
itailiors <itailior171@gmail.com>
2024-05-25 16:08:22 +0300

    stair walking and step go up

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    public Tilemap groundTilemap;
     // Start is called before the first frame update
+
+    private void Awake()
+    {
+        instance = this;
+    }
     void Start()
     {
         EventManager.OnTimerStart();
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -3,10 +3,11 @@ using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class playerInputs : MonoBehaviour
 {
-    [Header("RUN") ]
+    [Header("RUN")]
     [SerializeField] public float speed;
     [SerializeField] private float acceleration;
     [SerializeField] private float decceleration;
@@ -33,9 +34,15 @@ public class playerInputs : MonoBehaviour
     private float _fallSpeedYDampingChangeThreshold;
     private float lastGroundedTime;
     private bool isJumping = false;
-    private float gravityScale;  
+    private float gravityScale;
     private float xInput;
 
+    private Transform stepPos;
+    private Transform wallPos;
+
+    private bool bounce = false;
+    //private float moveSpeed = 0.2f; // Horizontal movement speed in units per second
+    private float moveDuration = 0.2f; // Duration of movement in seconds
 
 
     private void Awake()
@@ -48,21 +55,23 @@ public class playerInputs : MonoBehaviour
 
     private void Start()
     {
-        _fallSpeedYDampingChangeThreshold =  cameraManger.Instance._fallSpeedYDamoingChangeThreshold;
+        _fallSpeedYDampingChangeThreshold = cameraManger.Instance._fallSpeedYDamoingChangeThreshold;
+        stepPos = transform.Find("TileStepCheck").transform;
+        wallPos = transform.Find("WallStepCheck").transform;
     }
 
     void Update()
     {
         GetInput();
-        HandleJump();    
+        HandleJump();
         anim.SetFloat("speed", Mathf.Abs(xInput));
- 
+
         if (_rigidBody.velocity.y < _fallSpeedYDampingChangeThreshold && !cameraManger.Instance.isLerpingYDamping && !cameraManger.Instance.LerpedFromPlayerFalling)
         {
             cameraManger.Instance.LerpYDamping(true);
         }
 
-        if(_rigidBody.velocity.y >= 0f && !cameraManger.Instance.isLerpingYDamping && !cameraManger.Instance.LerpedFromPlayerFalling)
+        if (_rigidBody.velocity.y >= 0f && !cameraManger.Instance.isLerpingYDamping && !cameraManger.Instance.LerpedFromPlayerFalling)
         {
             cameraManger.Instance.LerpedFromPlayerFalling = false;
 
@@ -70,14 +79,14 @@ public class playerInputs : MonoBehaviour
         }
 
         #region Timer
-        lastGroundedTime -= Time.deltaTime;;
+        lastGroundedTime -= Time.deltaTime; ;
         #endregion
 
-        if(Mathf.Abs(_rigidBody.velocity.y) < jumpHangTimeThreshold)
+        if (Mathf.Abs(_rigidBody.velocity.y) < jumpHangTimeThreshold)
         {
             _rigidBody.gravityScale = gravityScale * jumpHangGravityMultiplier;
         }
-        else if(_rigidBody.velocity.y < 0)
+        else if (_rigidBody.velocity.y < 0)
         {
             _rigidBody.gravityScale = gravityScale * fallGravityMultiplier;
         }
@@ -90,10 +99,11 @@ public class playerInputs : MonoBehaviour
     private void FixedUpdate()
     {
         CheckGround();
+        CheckStep();
         ApplyFriction();
         MoveWithInput();
 
-        if(xInput != 0)
+        if (xInput != 0)
         {
             TurnCheck();
         }
@@ -113,8 +123,8 @@ public class playerInputs : MonoBehaviour
         float accelRate;
 
         if (lastGroundedTime > 0)
-         accelRate = (Mathf.Abs(targetSpeed) > 0) ? acceleration : decceleration;
-        else 
+            accelRate = (Mathf.Abs(targetSpeed) > 0) ? acceleration : decceleration;
+        else
             accelRate = (Mathf.Abs(targetSpeed) > 0) ? accelerationInAir : deccelerationInAir;
 
         float movement = Mathf.Pow(Mathf.Abs(speedDif) * accelRate, velPower) * Mathf.Sign(speedDif);
@@ -129,8 +139,8 @@ public class playerInputs : MonoBehaviour
             _rigidBody.AddForce(Vector2.up * jumpspeed, ForceMode2D.Impulse);
             lastGroundedTime = 0;
         }
-        
-        if(Input.GetKeyUp(KeyCode.Space) && _rigidBody.velocity.y > 0 )
+
+        if (Input.GetKeyUp(KeyCode.Space) && _rigidBody.velocity.y > 0)
         {
             _rigidBody.AddForce(Vector2.down * _rigidBody.velocity.y * (1 - jumpCutMultiplier), ForceMode2D.Impulse);
         }
@@ -146,7 +156,7 @@ public class playerInputs : MonoBehaviour
             _rigidBody.AddForce(Vector2.right * -amount, ForceMode2D.Impulse);
         }
     }
- 
+
     private void CheckGround()
     {
         anim.SetBool("grounded", Physics2D.OverlapAreaAll(groundcheck.bounds.min, groundcheck.bounds.max, groundMask).Length > 0);
@@ -157,10 +167,77 @@ public class playerInputs : MonoBehaviour
         }
         else isJumping = true;
     }
+    /*
+    private void CheckStep()
+    {
+        //Tilemap col = GameManager.instance.groundTilemap.GetComponent<Tilemap>();
+        var StepcircleBound = Physics2D.OverlapCircle(stepPos.position, 0.001f, groundMask);
+        var WallCircleBound = Physics2D.OverlapCircle(wallPos.position, 0.001f, groundMask);
+        if (StepcircleBound != null && WallCircleBound ==null && !bounce)
+        {
+            Vector2 currentPosition = transform.position;
+            float zRotation = transform.rotation.eulerAngles.z;
+            var roation = -1;
+            if (zRotation < 0) { roation = 1; }
+            var xmove = 0.2f * roation;
+            Vector2 newPosition = new Vector2(currentPosition.x + xmove, currentPosition.y + 0.3f);
+            transform.position = newPosition;
+            bounce = true;
+        }
+        else
+        {
+            bounce = false;
+        }
+    } */
+
+    private void CheckStep()
+    {
+        var StepcircleBound = Physics2D.OverlapCircle(stepPos.position, 0.001f, groundMask);
+        var WallCircleBound = Physics2D.OverlapCircle(wallPos.position, 0.001f, groundMask);
+
+        if (StepcircleBound != null && WallCircleBound == null && !bounce)
+        {
+            StartCoroutine(MovePlayerCoroutine()); // Start rotating the player
+            bounce = true;
+        }
+        else
+        {
+            bounce = false;
+        }
+    }
+
+    private IEnumerator MovePlayerCoroutine()
+    {
+        Vector2 currentPosition = transform.position;
+        float zRotation = transform.rotation.eulerAngles.y;
+        var roation = 1;
+        if ((zRotation % 360) >0) { roation = -1; }
+        float targetX = currentPosition.x + (0.2f*roation); // Move 0.2 units to the right
+        float targetY = currentPosition.y + 0.25f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < moveDuration)
+        {
+            // Interpolate between current and target positions
+            float newX = Mathf.Lerp(currentPosition.x, targetX, elapsedTime / moveDuration);
+            float newY = Mathf.Lerp(currentPosition.y, targetY, elapsedTime / moveDuration);
+            transform.position = new Vector2(newX, newY);
+
+            // Increment elapsed time
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        // Ensure the player reaches the target position exactly
+        transform.position = new Vector2(targetX, targetY);
+    }
+
+
 
     private void TurnCheck()
     {
-        if(xInput > 0 && !IsFacingRight)
+        if (xInput > 0 && !IsFacingRight)
         {
             Turn();
         }
@@ -172,7 +249,7 @@ public class playerInputs : MonoBehaviour
 
     private void Turn()
     {
-        if(IsFacingRight)
+        if (IsFacingRight)
         {
             Vector3 rotator = new Vector3(0f, 180f, 0f);
             transform.rotation = Quaternion.Euler(rotator);
@@ -189,6 +266,6 @@ public class playerInputs : MonoBehaviour
             _cameraFollowObject.CallTurn();
         }
     }
-    
+
 
 }
