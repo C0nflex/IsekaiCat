9396de6a5460d0d783b15b743ca2f56b210b2cc9
itailiors <itailior171@gmail.com>
2024-05-25 20:22:18 +0300

    some ruined my perfect code
    and i fixed

--- Assets/Scripts/playerInputs.cs
+++ Assets/Scripts/playerInputs.cs
@@ -212,7 +212,7 @@ public abstract class playerInputs : MonoBehaviour
         var roation = 1;
         if ((zRotation % 360) >0) { roation = -1; }
         float targetX = currentPosition.x + (0.2f*roation); // Move 0.2 units to the right
-        float targetY = currentPosition.y + 0.20f;
+        float targetY = currentPosition.y + 0.20f; // and thhis
         float elapsedTime = 0f;
 
         while (elapsedTime < moveDuration)
